6337c94127676f2643c0b3d1592bffa4c8623aaa
muhammadkh303@gmail.com <muhammadkh303@gmail.com>
2023-01-18 00:50:29 +0500

    issue check

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,7 @@ import HomeContent from "./homeContent";
 import BottomNavigation from "../../components/bottomNavigation";
 const Home=()=>{
     return (
-    <div className="flex flex-col relative h-[85vh] w-full  bg-white" >
+    <div className="flex flex-col relative h-screen w-full  bg-white" >
         <HomeHeader/>
         <HomeContent/>
         {/* <div className="mt-4"> */}
