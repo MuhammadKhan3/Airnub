2d8d08b5874150d88834c253f7bc918ba82b3d84
muhammadkh303@gmail.com <muhammadkh303@gmail.com>
2023-01-18 20:31:54 +0500

    resi asb

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './pages/home/home';
 function App() {
   return (
     // h-[90vh]
-  <div className='w-full h-full   '>
+  <div className='w-full h-screen   '>
     <Home/>
   </div>
 
